6a9303542a28ade9d1dada4bf4ec3d08d5d84fdf
Nirjla Shakya <shakyanirjala6@gmail.com>
2023-06-20 20:48:38 +0545

    Folders added

--- Practical1/tempCodeRunnerFile.java
+++
@@ -1,7 +0,0 @@
-umber to check whether it is prime or composite:");
-       int num = sc.nextInt();
-       sc.close();
-       int count=0;
-       for(int i=1;i<=num;i++)
-       {
-        if(num%i==0){
